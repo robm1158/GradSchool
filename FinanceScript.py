6f921f13e9a35f8295b3e37326ad5a706f11f417
Rob Mullins <mullins097956@gmail.com>
2019-11-21 01:12:48 -0700

    Adding url and API to file

--- FinanceScript.py
+++ FinanceScript.py
@@ -1,1 +1,5 @@
 # Starting the query of finance data 
+
+stockList = "CSCO"
+url = "https://www.alphavantage.co/query?function=TIME_SERIES_MONTHLY_ADJUSTED&symbol=" + stockList +"&apikey=FF4CIPERU65DWNNC"
+print(url)
