6c06fae2e49d5b9b878c1db65040651c1b44a8b9
robm1158 <mullins097956@gmail.com>
2020-03-02 14:46:18 -0700

    Adding writting mechinisim as well as adjusting logic

--- RMullinsLab1.java
+++ RMullinsLab1.java
@@ -4,7 +4,7 @@ If it is an apperand I will push the Letter to the stack class I made. If it is 
 I will follow certain rules laid out in the assignment to write commands to another file.*/
 
 import java.io.*;
-
+import java.util.Scanner;
 /*Bellow is a stack class. This class contains the methods to push and pop with the constructor.
 I will use this to hold the chars I parse from the input file.*/
 class stack{
@@ -27,15 +27,46 @@ class stack{
     }
 
 }
+class fileTasks{
+    public void createFile(String s){
+        try {
+            File myObj = new File(s);
+            if (myObj.createNewFile()) {
+              System.out.println("File created: " + myObj.getName());
+            } else {
+              System.out.println("File already exists.");
+            }
+          } catch (IOException e) {
+            System.out.println("An error occurred.");
+            e.printStackTrace();
+          }
+    }
+    public void writeFile(String fileName, String stringToWrite){
+        try {
+            FileWriter myWriter = new FileWriter(fileName,true);
+            BufferedWriter bw = new BufferedWriter(myWriter);
+            bw.write(stringToWrite);
+            //myWriter.write(stringToWrite);
+            bw.close();
+            System.out.println("Successfully wrote to the file.");
+          } catch (IOException e) {
+            System.out.println("An error occurred.");
+            e.printStackTrace();
+          }
+    }
+
+
+}
 
 
 /* postCOnvert will go through the logic of picking out the opperators and opperands.
 If it is a +-* /$ it will look at the stack and pop it twice, and then write the command to file.
 If it is an opperand it will get pushed to the stack right away.*/
 class postConvert{
-    public int convert(String s){
+    public void convert(String s){
         int len,set = 0,count = 0;
         len = s.length();
+        fileTasks write = new fileTasks();
         stack stackHold = new stack(len);
         for(int index = 0; index < len; index++){
             char letter = s.charAt(index);
@@ -43,15 +74,55 @@ class postConvert{
 Dont think it really provices anything or takes away from anything.*/
             if(letter == '+'){
                 count++;
-                int firstPop = stackHold.pop();
-                int secPop = stackHold.pop();
-                System.out.println("LD "+(char)(secPop)+"\n"+"AD "+ (char)(firstPop) + "\n"+"ST TEMP"+count+"\n" );
+                char firstPop = stackHold.pop();
+                char secPop = stackHold.pop();
+                stackHold.push((char)(count));
+
+                if(((int)(secPop) > 0 && (int)(secPop) <= 9) && ((int)(firstPop) > 0 && (int)(firstPop) <= 9)){
+                    write.writeFile("Output.txt", "LD TEMP"+(int)(secPop)+"\n"+"AD TEMP"+ (int)(firstPop) + "\n"+"ST TEMP"+count+"\n");
+                    System.out.println("LD TEMP"+(int)(secPop)+"\n"+"AD TEMP"+ (int)(firstPop) + "\n"+"ST TEMP"+count+"\n" );
+                }
+                
+                else if((int)(secPop) > 0 && (int)(secPop) <= 9){
+                    write.writeFile("Output.txt", "LD TEMP"+(int)(secPop)+"\n"+"AD "+ (char)(firstPop) + "\n"+"ST TEMP"+count+"\n");
+                    System.out.println("LD TEMP"+(int)(secPop)+"\n"+"AD "+ (char)(firstPop) + "\n"+"ST TEMP"+count+"\n" );
+                }
+                else if((int)(firstPop) > 0 && (int)(firstPop) <= 9){
+                    write.writeFile("output.txt", "LD "+(char)(secPop)+"\n"+"AD TEMP"+ (int)(firstPop) + "\n"+"ST TEMP"+count+"\n" );
+                    System.out.println("LD "+(char)(secPop)+"\n"+"AD TEMP"+ (int)(firstPop) + "\n"+"ST TEMP"+count+"\n" );
+                }
+                else{
+                    write.writeFile("Output.txt", "LD "+(char)(secPop)+"\n"+"AD "+ (char)(firstPop) + "\n"+"ST TEMP"+count+"\n");
+                    System.out.println("LD "+(char)(secPop)+"\n"+"AD "+ (char)(firstPop) + "\n"+"ST TEMP"+count+"\n" );
+
+                }
+
             }
             else if(letter == '-'){
                 count++;
                 char firstPop = stackHold.pop();
                 char secPop = stackHold.pop();
-                System.out.println("LD "+(char)(secPop)+"\n"+"SB "+ (char)(firstPop) + "\n"+"ST TEMP"+count+"\n" );
+                stackHold.push((char)(count));
+
+                if(((int)(secPop) > 0 && (int)(secPop) <= 9) && ((int)(firstPop) > 0 && (int)(firstPop) <= 9)){
+                    write.writeFile("Output.txt", "LD TEMP"+(int)(secPop)+"\n"+"SB TEMP"+ (int)(firstPop) + "\n"+"ST TEMP"+count+"\n");
+                    System.out.println("LD TEMP"+(int)(secPop)+"\n"+"SB TEMP"+ (int)(firstPop) + "\n"+"ST TEMP"+count+"\n" );
+                }
+                else if((int)(secPop) > 0 && (int)(secPop) <= 9){
+                    write.writeFile("Output.txt","LD TEMP"+(int)(secPop)+"\n"+"SB "+ (char)(firstPop) + "\n"+"ST TEMP"+count+"\n");
+
+                    System.out.println("LD TEMP"+(int)(secPop)+"\n"+"SB "+ (char)(firstPop) + "\n"+"ST TEMP"+count+"\n" );
+                }
+                else if((int)(firstPop) > 0 && (int)(firstPop) <= 9){
+                    write.writeFile("Output.txt","LD "+(char)(secPop)+"\n"+"SB TEMP"+ (int)(firstPop) + "\n"+"ST TEMP"+count+"\n");
+
+                    System.out.println("LD "+(char)(secPop)+"\n"+"SB TEMP"+ (int)(firstPop) + "\n"+"ST TEMP"+count+"\n" );
+                }
+                else{
+                    write.writeFile("Output.txt","LD "+(char)(secPop)+"\n"+"SB "+ (char)(firstPop) + "\n"+"ST TEMP"+count+"\n");
+
+                    System.out.println("LD "+(char)(secPop)+"\n"+"SB "+ (char)(firstPop) + "\n"+"ST TEMP"+count+"\n" );
+                }
 
             }
             else if(letter == '*'){
@@ -59,21 +130,72 @@ Dont think it really provices anything or takes away from anything.*/
                 char firstPop = stackHold.pop();
                 char secPop = stackHold.pop();
                 stackHold.push((char)(count));
-                System.out.println("LD "+(char)(secPop)+"\n"+"ML "+ (char)(firstPop) + "\n"+"ST TEMP"+count+"\n" );
+
+                if(((int)(secPop) > 0 && (int)(secPop) <= 9) && ((int)(firstPop) > 0 && (int)(firstPop) <= 9)){
+                    write.writeFile("Output.txt","LD TEMP"+(int)(secPop)+"\n"+"ML TEMP"+ (int)(firstPop) + "\n"+"ST TEMP"+count+"\n");
+                    System.out.println("LD TEMP"+(int)(secPop)+"\n"+"ML TEMP"+ (int)(firstPop) + "\n"+"ST TEMP"+count+"\n" );
+                }
+                
+                else if((int)(secPop) > 0 && (int)(secPop) <= 9){
+                    write.writeFile("Output.txt","LD "+(int)(secPop)+"\n"+"ML "+ (char)(firstPop) + "\n"+"ST TEMP"+count+"\n");
+                    System.out.println("LD "+(int)(secPop)+"\n"+"ML "+ (char)(firstPop) + "\n"+"ST TEMP"+count+"\n" );
+                }
+                else if((int)(firstPop) > 0 && (int)(firstPop) <= 9){
+                    write.writeFile("Output.txt","LD "+(char)(secPop)+"\n"+"ML TEMP"+ (int)(firstPop) + "\n"+"ST TEMP"+count+"\n");
+                    System.out.println("LD "+(char)(secPop)+"\n"+"ML TEMP"+ (int)(firstPop) + "\n"+"ST TEMP"+count+"\n" );
+                }
+                else{
+                    write.writeFile("Output.txt","LD "+(char)(secPop)+"\n"+"ML "+ (char)(firstPop) + "\n"+"ST TEMP"+count+"\n");
+                    System.out.println("LD "+(char)(secPop)+"\n"+"ML "+ (char)(firstPop) + "\n"+"ST TEMP"+count+"\n" );
+                }
             }
             else if(letter == '/'){
                 count++;
                 char firstPop = stackHold.pop();
                 char secPop = stackHold.pop();
                 stackHold.push((char)(count));
-                System.out.println("LD "+(char)(secPop)+"\n"+"DV "+ (char)(firstPop) + "\n"+"ST TEMP"+count+"\n" );
+
+                if(((int)(secPop) > 0 && (int)(secPop) <= 9) && ((int)(firstPop) > 0 && (int)(firstPop) <= 9)){
+                    write.writeFile("Output.txt","LD TEMP"+(int)(secPop)+"\n"+"DV TEMP"+ (int)(firstPop) + "\n"+"ST TEMP"+count+"\n" );
+                    System.out.println("LD TEMP"+(int)(secPop)+"\n"+"DV TEMP"+ (int)(firstPop) + "\n"+"ST TEMP"+count+"\n" );
+                }
+                else if((int)(secPop) > 0 && (int)(secPop) <= 9){
+                    write.writeFile("Output.txt","LD "+(int)(secPop)+"\n"+"DV "+ (char)(firstPop) + "\n"+"ST TEMP"+count+"\n" );
+                    System.out.println("LD "+(int)(secPop)+"\n"+"DV "+ (char)(firstPop) + "\n"+"ST TEMP"+count+"\n" );
+                }
+                else if((int)(firstPop) > 0 && (int)(firstPop) <= 9){
+                    write.writeFile("Output.txt","LD "+(char)(secPop)+"\n"+"DV TEMP"+ (int)(firstPop) + "\n"+"ST TEMP"+count+"\n");
+                    System.out.println("LD "+(char)(secPop)+"\n"+"DV TEMP"+ (int)(firstPop) + "\n"+"ST TEMP"+count+"\n" );
+                }
+                else{
+                    write.writeFile("Output.txt","LD "+(char)(secPop)+"\n"+"DV "+ (char)(firstPop) + "\n"+"ST TEMP"+count+"\n");
+                    System.out.println("LD "+(char)(secPop)+"\n"+"DV "+ (char)(firstPop) + "\n"+"ST TEMP"+count+"\n" );
+                }
                 
             }
+            /* Here I am implememnting my enhancment. I am including the $ which is a ^ in terms of math. */
             else if(letter == '$'){
                 count++;
                 char firstPop = stackHold.pop();
                 char secPop = stackHold.pop();
-                System.out.println("LD "+(char)(secPop)+"\n"+"EX "+ (char)(firstPop) + "\n"+"ST TEMP"+count+"\n" );
+                stackHold.push((char)(count));
+
+                if(((int)(secPop) > 0 && (int)(secPop) <= 9) && ((int)(firstPop) > 0 && (int)(firstPop) <= 9)){
+                    write.writeFile("Output.txt","LD TEMP"+(int)(secPop)+"\n"+"EX TEMP"+ (int)(firstPop) + "\n"+"ST TEMP"+count+"\n");
+                    System.out.println("LD TEMP"+(int)(secPop)+"\n"+"EX TEMP"+ (int)(firstPop) + "\n"+"ST TEMP"+count+"\n" );
+                }
+                else if((int)(secPop) > 0 && (int)(secPop) <= 9){
+                    write.writeFile("Output.txt","LD "+(int)(secPop)+"\n"+"EX "+ (char)(firstPop) + "\n"+"ST TEMP"+count+"\n" );
+                    System.out.println("LD "+(int)(secPop)+"\n"+"EX "+ (char)(firstPop) + "\n"+"ST TEMP"+count+"\n" );
+                }
+                else if((int)(firstPop) > 0 && (int)(firstPop) <= 9){
+                    write.writeFile("Output.txt","LD "+(char)(secPop)+"\n"+"EX TEMP"+ (int)(firstPop) + "\n"+"ST TEMP"+count+"\n");
+                    System.out.println("LD "+(char)(secPop)+"\n"+"EX TEMP"+ (int)(firstPop) + "\n"+"ST TEMP"+count+"\n" );
+                }
+                else{
+                    write.writeFile("Output.txt","LD "+(char)(secPop)+"\n"+"EX "+ (char)(firstPop) + "\n"+"ST TEMP"+count+"\n");
+                    System.out.println("LD "+(char)(secPop)+"\n"+"EX "+ (char)(firstPop) + "\n"+"ST TEMP"+count+"\n" );
+                }
                 
             }
             else{
@@ -82,8 +204,6 @@ Dont think it really provices anything or takes away from anything.*/
             }
             
         }
-        set = stackHold.pop();
-        return set;
     }
 }
 
@@ -92,17 +212,23 @@ the file. It will also instantiate the methods needed to run the program. */
 class RMullinsLab1{
 
     public static void main(String[] args)throws IOException{
-        String input;
         postConvert test = new postConvert();
+        fileTasks newFile = new fileTasks();
+        newFile.createFile("Output.txt");
         File file = new File("PostfixMachineLangInput.txt"); 
   
         BufferedReader br = new BufferedReader(new FileReader(file)); 
         
         String st; 
-        while ((st = br.readLine()) != null)
+        while ((st = br.readLine()) != null){
+            System.out.println("Result for "+st);
+            newFile.writeFile("Output.txt", "\n"+st+"\n");
+            test.convert(st);
+            //System.out.println();
+        }
+        br.close();
             
-            System.out.println("Result: "+test.convert(st));
-            System.out.println(st);
+            
     }
 
 }
