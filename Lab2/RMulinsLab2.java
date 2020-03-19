700d81f64522ef14155a3f2acb648dcd1f7cae44
Rob Mullins <mullins097956@gmail.com>
2020-03-19 01:36:46 -0600

    Adding an avg time of the recursive method

--- Lab2/RMulinsLab2.java
+++ Lab2/RMulinsLab2.java
@@ -9,11 +9,22 @@ class RMullinsLab2{
     public static void main(String[] args)throws IOException{
         recursiveHanoi test = new recursiveHanoi();
         ticTok time = new ticTok();
+        int index =0;
+        Long lasTime,avgTime,totalTime = (long)0;
+        for(index =0; index <30;index++){
+            System.out.println(index);
+            Long start = time.tic();
+            test.solve(4, 'A', 'C', 'B');
+            lasTime = (time.toc()-start);
+            System.out.println("time for this run "+lasTime);
+            System.out.println("total time "+totalTime);
+            totalTime = (totalTime + lasTime);
+        }
+        avgTime = totalTime/index;
+        System.out.println("Total avg time: "+avgTime);
         Long start = time.tic();
-        test.solve(20, 'A', 'C', 'B');
-        Long end = time.toc();
-        System.out.println(end-start);
-
+        test.solve(4, 'A', 'C', 'B');
+        System.out.println("Single run time "+(time.toc()-start));
     }
 
 
