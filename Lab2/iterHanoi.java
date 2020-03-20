29d0d09d756f225626137c3ec61fe795a56b3c28
Rob Mullins <mullins097956@gmail.com>
2020-03-19 20:58:11 -0600

    Adding iter version of hanoi and stack for int

---
+++ Lab2/iterHanoi.java
@@ -0,0 +1,35 @@
+package Lab2;
+
+java.lang.Math;
+
+public class iterHanoi{
+    private stackInt source,desto,spare;
+    //private int
+
+    public iterHanoi(int size) {
+        stackInt source = new stackInt(size);
+        stackInt desto = new stackInt(size);
+        stackInt spare = new stackInt(size);
+        //initialize with bigger rods on first
+        for (int iter = size; iter >= 1;iter--){
+            source.push(iter);
+        }
+
+    }
+
+    public void moveDisk(char toRod, char fromRod, int disk){
+        System.out.println("Move the disk "+disk + " from "+fromRod+" to "+toRod); 
+    }
+
+    public void solve(int num){
+        // We know this to be true so I will use this to my advantage
+        
+        int totalMoves = (int)(Math.pow(2, num) - 1);
+
+
+    }
+
+
+
+
+}
---
+++ Lab2/stackInt.java
@@ -0,0 +1,79 @@
+package Lab2;
+
+/*
+Author by Rob Mullins
+
+Bellow is a stack class. This class contains the methods to push, pop, resize, size, and 
+isEmpty with the constructor. I will use this to hold the ints I need to keep track of.*/
+public class stackInt{
+
+    //Here we set our class vars to hold information
+    private int[] container,copy;
+    private int top, max_size, curr_size;
+
+    //constructor for stackInt to intialize it
+    public stackInt(int max){
+        max_size = max;
+        container = new int[max_size];
+        top = -1;
+    }
+
+    //Basic push function to add values into the stack
+    public void push(int value){
+        if (curr_size >= max_size){
+            System.out.println("System overflow resize stack");
+            resize(max_size*2);
+        }
+        curr_size++;
+        container[++top] = value;
+
+
+    }
+
+    //Basic pop function to remove values into the stack
+
+    public int pop(){
+        if (top >= 0){
+            return(container[top--]);
+        }
+        else{
+            throw new RuntimeException("Stack underflow!!");
+        }
+        
+    }
+
+    // Returns the size of the stack
+    public int size(){
+        return top;
+    }
+    
+    //clears the entire stack
+    public void clear(){
+        for (int i = 0; i < top; i++)
+            container[i] = (Integer)null;
+        top = -1;
+    }
+
+    //Checks if the stack is empty
+    public boolean isEmpty(){
+        if(top == -1){
+            return true;
+        }
+        else{
+            return false;
+        }
+    }
+
+    //private function to resize the intially allocated stack so that we dont get over_flows
+    private void resize(int new_size){
+
+        copy = new int[new_size];
+        for(int i = 0; i < curr_size; i++){
+            copy[i] = container[i];
+        }
+        container = new int[new_size];
+        container = copy;
+        max_size = new_size;
+    }
+
+}
